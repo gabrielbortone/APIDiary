d87c0ea71078e22320d41410d79dab99394deb7c
Gabriel Bortone <newgabrielbortone@gmail.com>
2020-07-21 18:52:04 -0300

    Initial Implemetation part 2

---
+++ APIDiary/Repositories/EntradaRepository.cs
@@ -0,0 +1,12 @@
+﻿using APIDiary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDiary.Repositories
+{
+    //public class EntradaRepository : IEntradaRepository
+    //{
+    //}
+}
--- APIDiary/Repositories/IEntradaRepository.cs
+++ APIDiary/Repositories/IEntradaRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using APIDiary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,11 @@ namespace APIDiary.Repositories
 {
     public interface IEntradaRepository
     {
-
+        public void AddEntrada(Entrada entrada);
+        public void RemoveEntrada(int id);
+        public void UpdateEntrada(Entrada entrada);
+        public Entrada GetEntradaById(int? id);
+        public IEnumerable<Entrada> GetEntradasByDate(int id_user, DateTime data);
+        public IEnumerable<Entrada> GetAllEntrada(int id_user);
     }
 }
